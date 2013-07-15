bf94183bb05cf92739d08bb45b4af83ac82eeafa
Andor Bibók <bibok.andor@gmail.com>
2013-07-15 19:53:52 +0200

    külön config fájl

--- program/headers/global_var.h
+++ program/headers/global_var.h
@@ -1,10 +1,7 @@
-#ifdef __VISUALDSPVERSION__
-#define _COMPILE_WITH_BLACKFIN	// uncomment to compile with other compiler, some functions will be unavailable
-#endif
-
 #ifndef  _GLOBAL_VAR_H
 #define _GLOBAL_VAR_H
 
+#include "config.h"
 
 //--------------------------------------------------------------------------------------------------------
 // Header files
@@ -48,37 +45,16 @@
 // DMA flow mode
 #define FLOW_1					0x1000
 
-// UART buffersize
-#define UART_BUF_SIZE			100
-
 // audio buffersize
 #define AUDIO_BUF_SIZE			2				// 2 bytes for 24bit ADC/DAC
 
 // sampling rate of ADC
 #define F_SAMPLING				48000.0
 
-// parameters for preallocated data/coefficient arrays
-#define COEFF_SIZE				100
-#define DELAY_SIZE				100
-
-#define COEFF_TYPE				fract16
-#define DELAY_TYPE				fract16
-
 // math constants
 #define PI						3.14159265358979323846
 #define PIP2					1.57079632679489661923		// pi/2
 
-#define DEFAULT_BW_E0			1.0			// 1/sqrt(2) (~3dB) attenuation at corner freq
-#define DEFAULT_C1_E0			0.5			// ~1dB passband ripple
-#define DEFAULT_C2_E0			1.0			// 1/sqrt(2) (~3dB) attenuation at corner freq
-
-#define MAX_DB_INPUT			240
-#define MIN_LIN_INPUT			1e-12
-#define MAX_WS_INPUT			1200.0		// usable freq range: 20-24000Hz
-
-#define MIN_BANDWIDTH			100			// ~16Hz hasraütve
-#define MIN_FREQUENCY			0.001	//125			// ~20Hz
-#define MAX_FREQUENCY			150000		// ~24kHz
 
 #define CODE_DELIMITER			'*'
 
@@ -90,7 +66,7 @@
 #define MAX_REAL				9999999
 
 #define CLKIN					25000000
-#define BAUD_RATE			57600
+
 
 //--------------------------------------------------------------------------------------------------------
 // Typedefs and enums
