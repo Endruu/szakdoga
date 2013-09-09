40cffd00b2bab1acfe35de82ccabc18d43ed3036
Andor Bibók <bibok.andor@gmail.com>
2013-09-09 18:01:02 +0200

    átgondolt parser

--- program/source/communication.c
+++ program/source/communication.c
@@ -6,65 +6,147 @@ void out( char * string ) {
 	printf( string );
 }
 
-void parseInput( char * s ) {
-	int tmp = 0;
+void parseInputInfo( char * s, int l ) {
 
-	// to upper
-	while( s[tmp] != '\0' ) {	// +tmp limit!
-		if( s[tmp] >= 'a' && s[tmp] <= 'z' ) {
-			s[tmp] = s[tmp] - 32;
-		}
-		tmp++;
+	if( l > 3 ) {
+		//warn
+	} else if ( l == 3 && s[2] != CODE_DELIMITER ) {
+		//hiba
 	}
 
-	if( s[0] == '>' ) {			// change parameters
-	
-		if( s[1] == 'G' ) {		// generate filter
-			if( s[3] == 'I' ) {
-				// iir
-			}
-			else if( s[3] == 'F' ) {
-				// fir
-			}
-			else {
-				//hiba
-			}
-		}
-		else if( s[1] == 'C' ) {
-			// change channel
-		}
-		else {
-			//hiba
-		}
-		
+	if		( s[0] == 'C' && s[1] == 'W' ) {
+		// codeword
+		printCodeWord();
+	}
+	else if	( s[0] == 'C' && s[1] == 'U' ) {
+		// cpu usage
+		printCpuUsage();
+	}
+	else if	( s[0] == 'M' && s[1] == 'U' ) {
+		// memory usage
+		printMemoryUsage();
 	}
 	
-	else if( s[0] == '<' ) {	// request info
-	
-		if		( s[1] == 'C' && s[2] == 'W' ) {
-			// codeword
-			printCodeWord();
-		}
-		else if	( s[1] == 'C' && s[2] == 'U' ) {
-			// cpu usage
-			printTick( filterR );
-		}
-		else if	( s[1] == 'M' && s[2] == 'U' ) {
-			// memory usage
-			printMem( filterR );
-		}
-		else if	( s[1] == 'A' && s[2] == 'C' ) {
-			// actual channel
-			printActualChannel();
-		}
-		else {
-			//hiba
-		}
-		
+	// --- not yet implemented
+	else if	( s[0] == 'A' && s[1] == 'F' ) {
+		printActualFilter();
+	}
+	else if	( s[0] == 'R' && s[1] == 'I' ) {
+		printRoutingInfo();
 	}
 	
-	/* else if( s[0] == '?' ) { // testcase } */
 	else {
 		//hiba
 	}
+
+}
+
+void parseInputDebug( char * s, int l ) {
+	
+	if( l > 3 ) {
+		//warn
+	} else if ( l == 3 && s[2] != CODE_DELIMITER ) {
+		//hiba
+	}
+	
+	if ( s[0] == 'F' && s[1] == 'I' ) {
+		debugFilterInfo();
+	}
+	else {
+		//hiba
+	}
+	
+}
+
+void parseInputCommand( char * s, int l ) {
+
+	if( l > 2 && s[2] != ':' ) {
+		//hiba
+	}
+	
+	l -= 3;
+
+	if		( s[0] == 'G' && s[1] == 'F' ) {
+		cmdGenerateFilter( s + 3, l);
+	}
+	
+	// --- not yet implemented
+	else if	( s[0] == 'S' && s[1] == 'F' ) {
+		cmdSelectFilter( s + 3, l);
+	}
+	else if	( s[0] == 'S' && s[1] == 'O' ) {
+		// selects the filter on choosen output
+		cmdSelectOutput(  s + 3, l);
+	}
+	else if	( s[0] == 'R' && s[1] == 'F' ) {
+		cmdRouteFilter( s + 3, l);
+	}
+	
+	else {
+		//hiba
+	}
+
+}
+
+void parseInputTest( char * s, int l ) {
+
+	if( l > 2 && s[2] != ':' ) {
+		//hiba
+	}
+	
+	l -= 3;
+	
+	if		( s[0] == 'T' && s[1] == 'C' ) {
+		runTestcase( s + 3, l);
+	}
+	else if	( s[0] == 'I' && s[1] == 'R' ) {
+		runImpulse(  s + 3, l);
+	}
+	else if	( s[0] == 'R' && s[1] == 'S' ) {
+		runSimulation( s + 3, l);
+	}
+	else {
+		//hiba
+	}
+}
+
+void parseInput( char * s, int maxLength ) {
+	int length = 0;
+
+	// uppercase conversion and end checking
+	while( s[length] != '\0' ) {
+		if( length == maxLength ) {
+			//hiba
+		}
+		if( s[length] >= 'a' && s[length] <= 'z' ) {
+			s[length] = s[length] - 32;
+		}
+		length++;
+	}
+
+	length--;
+	
+	switch( s[0] ) {
+	
+		case '>' :
+			parseInputCommand( s+1, length );
+		break;
+		
+		case '<' :
+			parseInputInfo( s+1, length );
+		break;
+		
+		case '?' :
+			parseInputDebug( s+1, length );
+		break;
+		
+		case '#' :
+			parseInputTest( s+1, length );
+		break;
+		
+		default:
+		//hiba
+	
+	}
+	
 }
